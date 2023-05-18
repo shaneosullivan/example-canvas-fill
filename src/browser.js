8c2a4c16d844c46690e0dd783074cd902587da7b
Shane O'Sullivan <shaneosullivan1@gmail.com>
2023-05-18 13:51:33 +0100

    Add code to run a fast fill

--- src/browser.js
+++ src/browser.js
@@ -2,6 +2,7 @@
   const IMAGE_PATH = "./images/airplane.png";
   let fillSpeed = "slow";
   let selectedColour = "#FF0000";
+  let maskInfo = null;
 
   function runExample() {
     const canvas = document.getElementById("canvas");
@@ -41,21 +42,8 @@
     canvas.addEventListener("click", (evt) => {
       const { x, y } = getEventCoords(evt, canvas.getBoundingClientRect());
 
-      const dimensions = { height: canvas.height, width: canvas.width };
-      const imageData = getSrcImageData();
-
       console.log("x", x, "y", y);
-      worker.postMessage(
-        {
-          action: "fill",
-          dimensions,
-          imageData: imageData.data.buffer,
-          x,
-          y,
-          colour: selectedColour,
-        },
-        [imageData.data.buffer]
-      );
+      fillColour(x, y, colour, context);
     });
 
     // Set up the worker
@@ -70,6 +58,9 @@
       switch (data.response) {
         case "fill":
           handleFillMessageFromWorker(data, context);
+          break;
+        case "process":
+          handleProcessMessageFromWorker(data);
           break;
         default:
           console.error("Unknown response from worker", data);
@@ -95,6 +86,171 @@
 
     // Draw the full image
     context.drawImage(tempCanvas, 0, 0);
+  }
+
+  // We got data back from the Worker with the outside of the colouring
+  // image made opaque, but everything else made transparent.
+  // We will use this for when the user uses the 'fill' action on
+  // any pixel that is part of the background of the image
+  function handleProcessMessageFromWorker(data) {
+    const { height, width, allPixels: pixels } = data;
+    const pixelMaskInfo = data.pixelMaskInfo;
+
+    if (width !== canvas.width || height != canvas.height) {
+      // Outdated data, the screen has changed size, so
+      // ignore it
+      return;
+    }
+
+    const { canvas: tempCanvas, context: tempContext } = makeCanvas(
+      {
+        height,
+        width,
+      },
+      true
+    );
+
+    // set all to transparent black
+    tempContext.clearRect(0, 0, width, height);
+
+    const imageData = new ImageData(width, height);
+    imageData.data.set(new Uint8ClampedArray(pixels));
+
+    const { canvas: tempCanvas2, context: tempContext2 } = makeCanvas({
+      height,
+      width,
+    });
+    tempContext2.putImageData(imageData, 0, 0);
+
+    // Now we have just the data from the worker as the only
+    // non-transparent pixels in the image
+    tempContext.drawImage(tempCanvas2, 0, 0);
+
+    maskInfo = {
+      node: tempCanvas,
+      data: tempContext.getImageData(0, 0, width, height),
+      pixelMaskInfo,
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Compositing#globalcompositeoperation
+    // This is the magic incantation that gets all this canvas sorcery to work!!
+    // It makes it so that the fillRect() call later only fills in the non-transparent
+    // pixels and leaves the others transparent.  This way, only the background of the image is
+    // coloured in and the main subject is left as an empty 'mask' in this canvas.
+    // We can then easily use drawImage to place that masked image on top of the
+    // canvas the user is drawing
+    tempContext.globalCompositeOperation = "source-in";
+  }
+
+  function fillColour(x, y, colour, context) {
+    // Fill all the transparent pixels in the source image that is being
+    // coloured in
+
+    let contextForData = context;
+
+    // Only allow the background to be filled quickly if
+    // the user is colouring in a preselected image, as this
+    // will not change. If they are sketching, then
+    // the image is always changing and we cannot take the
+    // shortcut
+    const enableFastFill = fillSpeed === "instant";
+
+    x = Math.floor(x);
+    y = Math.floor(y);
+
+    // First check if this pixel is non-transparent in our cached
+    // image data. If so, instead of using the worker to perform
+    // an algorithmic fill, simply draw a rectangle with the right colour
+    // over the entire cached image data and draw that onto the
+    // main canvasNode.
+    if (maskInfo && enableFastFill) {
+      const firstIdx = getColorIndexForCoord(x, y, maskInfo.node.width);
+      const alphaValue = maskInfo.data.data[firstIdx + 3];
+
+      if (alphaValue > 0) {
+        // Yay, we can use the fast approach
+
+        // The alpha value in the maskInfo is an index into the pixelMaskInfo array.
+        // We subtract 1 from it as the number 0 tells us to NOT fill the pixel
+        const pixelMaskInfo = maskInfo.pixelMaskInfo[alphaValue - 1];
+        const { canvas: pixelMaskCanvasNode, context: pixelMaskContext } =
+          makeCanvas({
+            height: pixelMaskInfo.height,
+            width: pixelMaskInfo.width,
+          });
+
+        const pixelMaskImageData = new ImageData(
+          pixelMaskInfo.width,
+          pixelMaskInfo.height
+        );
+        pixelMaskImageData.data.set(
+          new Uint8ClampedArray(pixelMaskInfo.pixels)
+        );
+        pixelMaskContext.putImageData(pixelMaskImageData, 0, 0);
+
+        pixelMaskContext.globalCompositeOperation = "source-in";
+
+        pixelMaskContext.fillStyle = colour;
+
+        pixelMaskContext.fillRect(
+          0,
+          0,
+          pixelMaskInfo.width,
+          pixelMaskInfo.height
+        );
+
+        userContext.drawImage(
+          pixelMaskCanvasNode,
+          pixelMaskInfo.x,
+          pixelMaskInfo.y
+        );
+
+        storeUndoPoint();
+        callOnChange();
+
+        return;
+      }
+    }
+
+    // if a fill takes more than 10 seconds, theres likely a bug
+    // so let the user do another
+    const limit = Date.now() - 1000 * 10;
+    ongoingFills = ongoingFills.filter((time) => time > limit);
+    if (ongoingFills.length > 2) {
+      // If too many fills are queued, ignore this as it can crash
+      // the browser
+
+      return;
+    }
+
+    ongoingFills.push(Date.now());
+
+    const dimensions = {
+      height: imgCanvasNode.height,
+      width: imgCanvasNode.width,
+    };
+
+    const imageData = contextForData.getImageData(
+      0,
+      0,
+      dimensions.width,
+      dimensions.height
+    );
+
+    // Delegate the work of filling the image to the web worker.
+    // This puts it on another thread so large fills don't block the UI thread.
+    paintWorker.postMessage(
+      {
+        action: WorkerAction.FILL,
+        dimensions,
+        foreground: imageData.data.buffer,
+        isSketchFill: contextForData === userContext,
+        x,
+        y,
+        colour,
+      },
+      [imageData.data.buffer]
+    );
   }
 
   function makeCanvas(size) {
